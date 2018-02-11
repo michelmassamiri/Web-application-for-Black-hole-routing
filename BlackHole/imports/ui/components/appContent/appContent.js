a92da10638b37a1c5f6aaa964bf680cf9afdab76
michel massamiri <mmassamire@services.emi.u-bordeaux.fr>
2018-02-11 14:33:15 +0100

    Importing templates

--- BlackHole/imports/ui/components/appContent/appContent.js
+++ BlackHole/imports/ui/components/appContent/appContent.js
@@ -1,3 +1,7 @@
 import { Template } from 'meteor/templating';
 
+
+import '../addRoute/addRoute.js';
+import '../listCommand/listCommand.js';
+import '../listRoutes/listRoutes.js';
 import './appContent.html';
